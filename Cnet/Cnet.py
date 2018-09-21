18fec8709b24f8e2e96e175cc4bde2acb47d4daa
Christian <chriamue@gmail.com>
2018-09-21 17:58:19 +0200

    up down blocks

--- Cnet/Cnet.py
+++ Cnet/Cnet.py
@@ -1,24 +1,42 @@
 from keras.models import Model, Sequential
-from keras.layers import Input, Dense, Conv2D, Reshape
+from keras.layers import Input, Dense, Conv2D, Reshape, MaxPooling2D, UpSampling2D, Dropout, BatchNormalization
 
 
 class Cnet(Model):
     def __repr__(self):
         print('Cnet')
 
-    def __init__(self, inputs=Input(shape=(256, 256, 1)), classes=2, dropout=0.5 ):
+    def __init__(self, inputs=Input(shape=(256, 256, 1)), classes=2, dropout=0.5):
 
         self.dropout = dropout
-        self.block1 = self.block(32, 128,inputs)
-        self.block2 = self.block(128, 256,self.block1)
-        self.block3 = self.block(256, 64,self.block2)
-        self.out = Conv2D(1, (1, 1), activation='sigmoid')(self.block2)
+        self.kernel_initializer = 'he_normal'
+        self.activation = 'relu'
+        self.block1 = self.block(4, 32, inputs)
+        self.block2 = self.block(32, 128, self.block1)
+        self.block3 = self.block(128, 32, self.block2)
+        self.block4 = self.block(32, 4, self.block3)
+        self.out = Conv2D(1, (1, 1), activation='sigmoid')(self.block4)
         super(Cnet, self).__init__(inputs=inputs, output=self.out)
 
     def block(self, input_filters, output_filters, input_layer):
-        conv1 = Conv2D(input_filters, (3,3), activation='relu', padding='same',
-                       kernel_initializer='he_normal')(input_layer)
-        conv2 = Conv2D(input_filters*2, (3,3), activation='sigmoid', padding='same')(conv1)
-        conv3 = Conv2D(input_filters*4, (3,3), activation='sigmoid', padding='same')(conv2)
-        out = Conv2D(output_filters, (1, 1), activation='sigmoid')(conv3)
+        conv1 = Conv2D(input_filters, (3, 3), activation=self.activation, padding='same',
+                       kernel_initializer=self.kernel_initializer)(input_layer)
+        conv2 = Conv2D(input_filters*2, (3, 3), activation=self.activation,
+                       padding='same', kernel_initializer=self.kernel_initializer)(conv1)
+        conv3 = Conv2D(input_filters*4, (3, 3), activation=self.activation,
+                       padding='same', kernel_initializer=self.kernel_initializer)(conv2)
+        drop1 = Dropout(self.dropout)(conv3)
+        bn = BatchNormalization()(drop1)
+        return bn
+
+    def downblock(self, input_filters, output_filters, input_layer):
+        b = self.block(input_filters, 2*output_filters, input_layer)
+        pool1 = MaxPooling2D(pool_size=(2, 2))(b)
+        out = Conv2D(output_filters, (3, 3), activation=self.activation)(pool1)
+        return out
+
+    def upblock(self, input_filters, output_filters, input_layer):
+        b = self.block(input_filters, 2*output_filters, input_layer)
+        upsample = UpSampling2D(size=(2, 2))(b)
+        out = Conv2D(output_filters, (3, 3), activation=self.activation)(upsample)
         return out
